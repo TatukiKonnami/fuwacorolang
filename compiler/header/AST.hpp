b8ea3c304947f1eee501e6a6b464a5eb2ca980e5
TatukiKonnami <dmb1703@stumail.daido-it.ac.jp>
2017-11-15 00:55:59 +0900

    create AST.hpp first

---
+++ compiler/header/AST.hpp
@@ -0,0 +1,18 @@
+#ifndef AST_HPP
+#define AST_HPP
+
+
+#include <string>
+#include <map>
+#include <vector>
+#include "llvm/Support/Casting.h"
+#include "APP.hpp"
+
+enum AstID
+{
+    BaseID
+};
+
+/**
+ * ASTの基底クラス
+ */
