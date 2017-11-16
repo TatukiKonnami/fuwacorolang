b38dd86fdefca13fcb5ec872d6eae6815675a4de
TatukiKonnami <dmb1703@stumail.daido-it.ac.jp>
2017-11-16 19:07:47 +0900

    create BaseAst class

--- compiler/header/AST.hpp
+++ compiler/header/AST.hpp
@@ -16,3 +16,11 @@ enum AstID
 /**
  * ASTの基底クラス
  */
+class BaseAST{
+    AstID ID;
+
+    public:
+    BaseAST(AstID id):ID(id){}
+    virtual ~BaseAST(){}
+    AstID getValueID() const {return ID;}
+};
